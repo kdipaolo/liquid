c2be498db86982638229c2afeeb3f82293a9809e
Kurt DiPaolo <kurtdipaolo@gmail.com>
2015-03-26 15:21:10 -0400

    Added JS waypoints

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,18 @@
 /* jshint devel:true */
 console.log('\'Allo \'Allo!');
+
+
+var waypoint = new Waypoint({
+  element: document.getElementById('markets'),
+  handler: function(direction) {
+  if(direction === "down"){     
+    $('.header').addClass('slidedown');
+    $('#markets').addClass('ani');
+  }else {
+  
+   $('.header').removeClass('slidedown');
+}
+  }
+})
+
+
